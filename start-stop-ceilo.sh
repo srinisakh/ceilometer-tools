8233d7ded2367d5f88194a72864e583d8b0da684
srsakhamuri <srsakhamuri>
2015-02-26 21:51:19 +0000

    Cielo services start and stop

---
+++ start-stop-ceilo.sh
@@ -0,0 +1,7 @@
+sudo service ceilometer-collector $1
+sudo service ceilometer-agent-notification $1
+sudo service ceilometer-alarm-notifier $1
+sudo service ceilometer-agent-compute $1
+sudo service ceilometer-alarm-evaluator $1
+sudo service ceilometer-agent-central $1
+#sudo service ceilometer-api $1
