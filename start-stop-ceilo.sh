48981d7cd06557c7cf1e1018615320701d4e3432
srsakhamuri <sr.sakhamuri@gmail.com>
2015-03-24 09:46:51 -0600

    Update start-stop-ceilo.sh

--- start-stop-ceilo.sh
+++ start-stop-ceilo.sh
@@ -5,3 +5,6 @@ sudo service ceilometer-agent-compute $1
 sudo service ceilometer-alarm-evaluator $1
 sudo service ceilometer-agent-central $1
 sudo service ceilometer-api $1
+# If using apaceh2 on Helion
+a2ensite ceilometer_modwsgi.conf
+service apache2 reload
