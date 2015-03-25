e6440aca4a6bace21145c673cc4d5d06fb84669a
srsakhamuri <sr.sakhamuri@gmail.com>
2015-03-25 10:11:12 -0600

    Update retry_test.py

--- retry_test.py
+++ retry_test.py
@@ -1,30 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2012 New Dream Network, LLC (DreamHost)
-#
-# Author: Doug Hellmann <doug.hellmann@dreamhost.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
-
-"""Command line tool for creating test data for Ceilometer.
-
-Usage:
-
-Generate testing data for e.g. for default time span
-
-source .tox/py27/bin/activate
-./tools/make_test_data.py --user 1 --project 1 1 cpu_util 20
-"""
 from __future__ import print_function
 
 import argparse
