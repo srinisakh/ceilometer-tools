9bdf72f136b88fe3f30f66e910698c8b9acd1f25
srsakhamuri <sr.sakhamuri@gmail.com>
2015-04-10 21:44:04 -0600

    Update openstack-dev-pkgs.sh

--- openstack-dev-pkgs.sh
+++ openstack-dev-pkgs.sh
@@ -1,5 +1,6 @@
 sudo apt-get -y update
 sudo apt-get -y install git
+sudo apt-get -y install vim
 sudo apt-get -y install ansible
 git clone http://github.com/srsakhamuri/ceilometer-tools
 ansible-playbook -i "localhost," -c local ceilometer-tools/ansible-dev-playbook.yml
