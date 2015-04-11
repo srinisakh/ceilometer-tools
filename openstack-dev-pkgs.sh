ad5dfb8a9b642a78ff304c4cb6a4388376394f0e
srsakhamuri <sr.sakhamuri@gmail.com>
2015-04-10 21:27:57 -0600

    Update openstack-dev-pkgs.sh

--- openstack-dev-pkgs.sh
+++ openstack-dev-pkgs.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 sudo apt-get install git
 sudo apt-get install ansible
 git clone http://github.com/srsakhamuri/ceilometer-tools
-ansible-playbook -i "localhost," -c local ansible-dev-playbook.yml
+ansible-playbook -i "localhost," -c local ceilometer-tools/ansible-dev-playbook.yml
