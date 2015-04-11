798af88d76808cea83a23bcf1c5584a899cb3e49
srsakhamuri <sr.sakhamuri@gmail.com>
2015-04-10 22:03:41 -0600

    Update openstack-dev-pkgs.sh

--- openstack-dev-pkgs.sh
+++ openstack-dev-pkgs.sh
@@ -1,5 +1,6 @@
 sudo apt-get -y update
 sudo apt-get -y install git
+sudo apt-get -y install git-review
 sudo apt-get -y install vim
 sudo apt-get -y install ansible
 git clone http://github.com/srsakhamuri/ceilometer-tools
